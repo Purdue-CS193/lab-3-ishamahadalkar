9bfdb96eea72226ae97d887002d065ba1f188095
ishamahadalkar <imahadal@purdue.edu>
2018-10-02 14:32:33 -0400

    Add manual test of StringUtils in main

--- src/MathUtils.java
+++ src/MathUtils.java
@@ -3,7 +3,7 @@ public class MathUtils {
      * divideNumbers -- a / b
      * Divide a by b and return the result
      */
-    public static double divideNumbers(int dividend, int divisor) {
+    public static double divideNumbers(double dividend, double divisor) {
         /* Do some casual division ... Super easy ... Shouldn't be any problems here */
         double res = dividend / divisor;
         /* Return the DEFINITELY CORRECT result */
@@ -19,17 +19,20 @@ public class MathUtils {
         int res = base;
 
         /* Determine if the exponent is negative */
-        boolean isNegative = false;
+        boolean isNegative;
         if (exp < 0) {
             exp *= -1;
             isNegative = true;
+        } else {
+            isNegative = false;
         }
+        System.out.println(isNegative);
 
         /* Multiply the result by the base exp times
             Ex: base = 2, exp = 2
                --> res = 2, then res = 4 */
         for (int i = 0; i < exp; i++) {
-           res *= base; 
+           res = res * base;
         }
 
         /* If it's a negative exponent, we should invert it! */
